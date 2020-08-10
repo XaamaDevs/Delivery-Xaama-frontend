d4f8f05425e37601cff557b93402bd4cf08f934f
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-08-10 15:30:34 -0300

    Routes

---
+++ src/pages/Order/index.js
@@ -0,0 +1,12 @@
+//	Importing React main module and its features
+import React, { useState } from "react";
+
+//	Importing React Router features
+import { Link, useHistory } from "react-router-dom";
+
+//	Exporting resource to routes.js
+export default function Order() {
+	return (
+		<div>Teste</div>
+	);
+}
