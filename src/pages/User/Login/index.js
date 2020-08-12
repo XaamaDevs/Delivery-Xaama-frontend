d7144021d187f176ba977f69838ab3549fab5383
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2020-08-12 13:52:39 -0300

    Login created

--- src/pages/User/Login/index.js
+++ src/pages/User/Login/index.js
@@ -4,9 +4,74 @@ import React, { useState } from "react";
 //	Importing React Router features
 import { Link, useHistory } from "react-router-dom";
 
+// Importing styles
+//import "./styles.css";
+
 //	Exporting resource to routes.js
-export default function Login() {
+export default function Signup() {
+
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+	const [passwordC, setPasswordC] = useState("");
+	const [thumbnail, setThumbnail] = useState(null);
+
+	function handleSubmit() {
+
+	}
+
 	return (
-		<div>Login</div>
+		<div className="user-container d-flex h-100">
+			<div className="col-md-3 py-3 m-auto text-white">
+				<form onSubmit={handleSubmit}>
+					<div className="row my-1">
+						<div className="col my-1">
+							<label>Email: </label>
+							<input 
+								type="email" 
+								className="form-control" 
+								name="email" 
+								placeholder="email@provedor.com" 
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+								required
+							/>
+						</div>
+					</div>
+					<div className="row my-1">
+						<div className="col my-1">
+							<label>Senha: </label>
+							<input 
+								type="password" 
+								className="form-control" 
+								name="password" 
+								placeholder="Senha" 
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+								required
+							/>
+						</div>
+					</div>
+					<div className="row my-1">
+						<div className="col text-center">
+							<small>Não tem conta? </small>
+							<Link className="text-light" to="/signup">
+								<small>Clique aqui</small>
+							</Link>
+							<small> para se cadastrar</small>
+						</div>
+					</div>
+					<div className="row my-3">
+						<div className="col text-center">
+							<input 
+								type="submit" 
+								className="btn btn-warning btn-md" 
+								value="Acessar"
+							/>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
 	);
 }
