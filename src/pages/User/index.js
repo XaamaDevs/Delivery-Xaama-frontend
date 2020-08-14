94a3e8d25c296f9fc4fb128cfb11d86543a0a1bf
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-08-13 22:00:57 -0300

    User profile

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -4,9 +4,33 @@ import React, { useState } from "react";
 //	Importing React Router features
 import { Link, useHistory } from "react-router-dom";
 
+//	Importing React features
+import Image from 'react-bootstrap/Image';
+import { Container, Card, ListGroup, Col, Row, Button } from "react-bootstrap";
+
+import image from "../../assets/background2.jpg";
+
 //	Exporting resource to routes.js
 export default function User() {
 	return (
-		<div>User</div>
+		<div className="user-container h-100">
+      <div className="d-flex flex-row flex-wrap h-100">
+        <div className="d-flex col-sm-1 flex-column m-auto p-3">
+         <Image src={image} fluid />
+        </div>
+        <div className="col-sm-4 m-auto p-3">
+          <Card style={{ width: "18rem" }}>
+            <Card.Header>Diego</Card.Header>
+            <ListGroup variant="flush">
+              <ListGroup.Item>diego.t.queiroz@gmail.com</ListGroup.Item>
+              <ListGroup.Item>(32) 9 9948-2731</ListGroup.Item>
+              <ListGroup.Item>Rua padre Serafim - 128</ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </div>
+      </div>
+      <Button variant="outline-warning">Editar perfil</Button>{" "}
+      <Button variant="outline-danger">Apagar perfil</Button>
+    </div>
 	);
 }
