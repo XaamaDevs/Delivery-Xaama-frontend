67f0367d1805204089cbbcb24f29d39939ce4da5
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2020-08-14 16:52:42 -0300

    User profile edit

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,36 +1,57 @@
 //	Importing React main module and its features
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 //	Importing React Router features
 import { Link, useHistory } from "react-router-dom";
 
 //	Importing React features
-import Image from 'react-bootstrap/Image';
-import { Container, Card, ListGroup, Col, Row, Button } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import { Container, Card, ListGroup, Button } from "react-bootstrap";
 
-import image from "../../assets/background2.jpg";
+// Importing backend api
+import api from "../../services/api";
+
 
 //	Exporting resource to routes.js
 export default function User() {
+	const [user, setUser] = useState([]);
+
+	const userId = sessionStorage.getItem("userId");
+
+	useEffect(() => {
+		async function loadUser() {
+			const response = await api.get("/user/" + userId);
+			setUser(response.data);
+		}
+
+		loadUser();
+	}, []);
+
+
 	return (
 		<div className="user-container h-100">
-      <div className="d-flex flex-row flex-wrap h-100">
-        <div className="d-flex col-sm-1 flex-column m-auto p-3">
-         <Image src={image} fluid />
-        </div>
-        <div className="col-sm-4 m-auto p-3">
-          <Card style={{ width: "18rem" }}>
-            <Card.Header>Diego</Card.Header>
-            <ListGroup variant="flush">
-              <ListGroup.Item>diego.t.queiroz@gmail.com</ListGroup.Item>
-              <ListGroup.Item>(32) 9 9948-2731</ListGroup.Item>
-              <ListGroup.Item>Rua padre Serafim - 128</ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </div>
-      </div>
-      <Button variant="outline-warning">Editar perfil</Button>{" "}
-      <Button variant="outline-danger">Apagar perfil</Button>
-    </div>
+			<div className="d-flex flex-row flex-wrap h-100">
+				<div className="col-sm-4 m-auto p-3">
+					<Image src={user.thumbnail_url} fluid/>
+					<br/> <br/>
+					<Button variant="outline-warning">Trocar foto</Button>{" "}
+					<Button variant="outline-danger">Apagar foto</Button>
+				</div>
+				<div className="col-sm-4 m-auto p-3">
+					<Card style={{ width: "23rem" }}>
+						<Card.Header>{user.name}</Card.Header>
+						<ListGroup variant="flush">
+							<ListGroup.Item>{user.email}</ListGroup.Item>
+							<ListGroup.Item>{user.phone ? user.phone: "Telefone: (__) _ ____-____"}</ListGroup.Item>
+							<ListGroup.Item>{user.address ? user.address: "Endreço:"}</ListGroup.Item>
+						</ListGroup>
+					</Card>
+					<br/>
+					<Button variant="outline-warning">Editar perfil</Button> {" "}
+					<Button variant="outline-danger">Trocar senha</Button> {" "}
+					<Button variant="outline-danger">Apagar perfil</Button>
+				</div>
+			</div>
+		</div>
 	);
 }
