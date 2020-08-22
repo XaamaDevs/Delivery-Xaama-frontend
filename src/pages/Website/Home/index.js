47b18e4cbb5fb69df7a2a3ca3455089fdaf798e0
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2020-08-22 14:07:23 -0300

    Home and Not Found pages have been converted to React Bootstrap

--- src/pages/Website/Home/index.js
+++ src/pages/Website/Home/index.js
@@ -4,11 +4,14 @@ import React from "react";
 //	Importing React Router features
 import { Link } from "react-router-dom";
 
+//	Importing React Bootstrap features
+import { Jumbotron, Carousel, Image } from "react-bootstrap";
+
 //	Exporting resource to routes.js
 export default function Home() {
 	return (
 		<div className="website-container d-flex flex-row flex-wrap align-items-center m-0 p-0 h-100">
-			<div className="jumbotron col-sm-6 m-auto p-3 pl-5 bg-transparent">
+			<Jumbotron className="col-sm m-auto p-0 pl-5 bg-transparent">
 				<h1 className="display-5 font-italic text-white">Está com fome?</h1>
 				<h1 className="display-1 text-white">Não espere!</h1>
 				<p className="font-italic text-warning">Faça seu pedido agora</p>
@@ -20,29 +23,33 @@ export default function Home() {
 						Veja o Cardápio
 					</Link>
 				</p>
-			</div>
-
-			<div id="carouselExampleControls" className="carousel slide m-auto" data-ride="carousel">
-				<div className="carousel-inner">
-					<div className="carousel-item active">
-						<img className="w-100" src="https://cdn.iconscout.com/icon/free/png-256/fast-food-1851561-1569286.png" alt="First slide"/>
-					</div>
-					<div className="carousel-item">
-						<img className="w-100" src="https://gormansfamilyfood.com/files/2020/04/003-drink.png" alt="Second slide"/>
-					</div>
-					<div className="carousel-item">
-						<img className="w-100" src="https://plazaoaxacallc.com/files/2019/06/001_taco.png" alt="Third slide"/>
-					</div>
-				</div>
-				<a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-					<span className="carousel-control-prev-icon" aria-hidden="true"></span>
-					<span className="sr-only">Previous</span>
-				</a>
-				<a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-					<span className="carousel-control-next-icon" aria-hidden="true"></span>
-					<span className="sr-only">Next</span>
-				</a>
-			</div>
+			</Jumbotron>
+			<Carousel className="col-sm-5 m-auto p-5">
+				<Carousel.Item>
+					<Image 
+						className="w-100" 
+						src="https://cdn.iconscout.com/icon/free/png-256/fast-food-1851561-1569286.png" 
+						alt="First slide"
+						fluid
+					/>
+				</Carousel.Item>
+				<Carousel.Item>
+					<Image 
+						className="w-100" 
+						src="https://gormansfamilyfood.com/files/2020/04/003-drink.png" 
+						alt="Second slide"
+						fluid
+					/>
+				</Carousel.Item>
+				<Carousel.Item>
+					<Image 
+						className="w-100" 
+						src="https://plazaoaxacallc.com/files/2019/06/001_taco.png" 
+						alt="Third slide"
+						fluid
+					/>
+				</Carousel.Item>
+			</Carousel>
 		</div>
 	);
 }
--- src/pages/Website/NotFound/index.js
+++ src/pages/Website/NotFound/index.js
@@ -4,11 +4,14 @@ import React from "react";
 //	Importing React Router features
 import { Link } from "react-router-dom";
 
+//	Importing React Bootstrap features
+import { Jumbotron } from "react-bootstrap";
+
 //	Exporting resource to routes.js
 export default function NotFound() {
 	return (
 		<div className="website-container d-flex flex-row flex-wrap align-items-center m-0 p-0 h-100">
-			<div className="jumbotron ml-3 p-3 pl-5 bg-transparent">
+			<Jumbotron className="col-sm ml-3 p-3 bg-transparent">
 				<h1 className="display-5 font-italic text-white">Erro 404</h1>
 				<h1 className="display-1 text-white">Página não encontrada</h1>
 				<p className="font-italic text-warning">O conteúdo que você está procurando não existe</p>
@@ -20,7 +23,7 @@ export default function NotFound() {
 						Voltar para o início
 					</Link>
 				</p>
-			</div>
+			</Jumbotron>
 		</div>
 	);
 }
