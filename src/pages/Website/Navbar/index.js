af45ab73d6f35d7728f2fb58ee57d8228892a168
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2020-09-17 10:02:15 -0300

    Fixing product card on shopping basket

--- src/pages/Website/Navbar/index.js
+++ src/pages/Website/Navbar/index.js
@@ -128,31 +128,38 @@ export default function WebsiteNavbar({ userId, setUserId, user, setUser, order,
 									/>
 								</Col>
 								<Col sm>
-									<Card.Body key={product._id}>
-										<Card.Title>{product.product ? product.product.name : null }</Card.Title>
-										<Card.Text>
-											{product.product ? ((product.product.ingredients.length === 1) ?
-												"Ingrediente: "
-												:
-												"Ingredientes: "
-											)
-												:
-												null
-											}
-											{product.product ? product.product.ingredients.map((ingredient, index) => (
-												index === product.product.ingredients.length-1 ?
-													ingredient
+									<Row>
+										<Card.Body>
+											<Card.Title>{product.product ? product.product.name : null }</Card.Title>
+											<Card.Text>
+												{product.product ? ((product.product.ingredients.length === 1) ?
+													"Ingrediente: "
 													:
-													ingredient + ", "
-											))
-												:
-												null
-											}
-										</Card.Text>
-										<Card.Text>
-											{product && product.note ? "Observações: " + product.note : "Sem Observações"}
-										</Card.Text>
-									</Card.Body>
+													"Ingredientes: "
+												)
+													:
+													null
+												}
+												{product.product ? product.product.ingredients.map((ingredient, index) => (
+													index === product.product.ingredients.length-1 ?
+														ingredient
+														:
+														ingredient + ", "
+												))
+													:
+													null
+												}
+											</Card.Text>
+										</Card.Body>
+									</Row>
+									<Row>
+										<Card.Body>
+											<Card.Title>Observações:</Card.Title>
+											<Card.Text>
+												{product.note ? product.note : "Sem Observações"}
+											</Card.Text>
+										</Card.Body>
+									</Row>
 								</Col>
 							</Row>
 							<Row>
