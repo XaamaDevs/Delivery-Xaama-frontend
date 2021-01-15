092faacadac751a0a7ad89fe8df9bde2d08f972c
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-01-15 09:46:35 -0300

    Fixing user page buttons width

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -583,7 +583,7 @@ export default function User({ userId, setUserId, user, setUser, companyInfo, se
 											onClick={() => history.push("/allorders")}
 											className="mx-1 my-2 w-100"
 											id="btn-custom-outline"
-											sm="3"
+											sm="4"
 										>
 											Listar pedidos
 										</Button>
@@ -594,7 +594,7 @@ export default function User({ userId, setUserId, user, setUser, companyInfo, se
 													className="mx-1 my-2 w-100"
 													variant="outline-warning"
 													onClick={() => setModalImages(true)}
-													sm="4"
+													sm="3"
 												>
 													Editar imagens
 												</Button>
