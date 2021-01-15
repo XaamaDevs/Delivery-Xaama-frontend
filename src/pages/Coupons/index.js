b1c5ff5a0fbc0208b892e1164dd2ad69f5b773fe
ThiagoPereiraUFV <thiago.pereira.98@outlook.com>
2021-01-15 16:57:03 -0300

    Coupon add and update finished

--- src/pages/Coupons/index.js
+++ src/pages/Coupons/index.js
@@ -26,23 +26,30 @@ import api from "../../services/api";
 //	Exporting resource to routes.js
 export default function Coupons({ userId, companyInfo }) {
 	//	Coupon state variables
-	const couponTypes = ["quantidade", "privado", "valor", "frete"];
+	const couponTypes = ["quantidade", "valor", "frete"];
 	const couponMethods = ["dinheiro", "porcentagem"];
 	const [coupons, setCoupons] = useState([]);
 	const [coupon, setCoupon] = useState({});
 	const [couponsByType, setCouponsByType] = useState({});
-	const [couponUserId, setCouponUserId] = useState([]);
+	const [couponUserId, setCouponUserId] = useState("");
 	const [couponName, setCouponName] = useState("");
 	const [couponType, setCouponType] = useState("");
 	const [couponQty, setCouponQty] = useState("");
 	const [couponMethod, setCouponMethod] = useState("");
-	const [couponDiscount, setCouponDiscount] = useState(0);
+	const [couponDiscount, setCouponDiscount] = useState(null);
+	const [couponMinValue, setCouponMinValue] = useState(null);
 	const [couponAvailable, setCouponAvailable] = useState(false);
+	const [couponPrivate, setCouponPrivate] = useState(false);
+
+	//	Users state variables
+	const [users, setUsers] = useState([]);
+
+	//	Modal state variables
+	const [modalAlert, setModalAlert] = useState(false);
 	const [modalCouponAdd, setModalCouponAdd] = useState(false);
 	const [modalCouponUpdate, setModalCouponUpdate] = useState(false);
 
 	//	Message settings
-	const [modalAlert, setModalAlert] = useState(false);
 	const [title, setTitle] = useState("");
 	const [message, setMessage] = useState("");
 	const [toastShow, setToastShow] = useState(false);
@@ -50,19 +57,21 @@ export default function Coupons({ userId, companyInfo }) {
 
 	//	Update coupon state variables
 	useEffect(() => {
-		setCouponUserId(coupon ? coupon.userId : []);
+		setCouponUserId(coupon ? coupon.private ? users.find(u => u._id === coupon.userId).email : null  : "");
 		setCouponName(coupon ? coupon.name : "");
 		setCouponType(coupon ? coupon.type : "");
 		setCouponQty(coupon ? coupon.qty : "");
 		setCouponMethod(coupon ? coupon.method : "");
 		setCouponDiscount(coupon ? coupon.discount : null);
+		setCouponMinValue(coupon ? coupon.minValue : null);
 		setCouponAvailable(coupon ? coupon.available : false);
+		setCouponPrivate(coupon ? coupon.private : false);
 	}, [modalCouponAdd, modalCouponUpdate]);
 
-	//	Loading coupons list by type
+	//	Loading coupons list by type and all users
 	useEffect(() => {
 		async function fetchData() {
-			api.get("couponAll", {
+			await api.get("couponAll", {
 				headers: {
 					"x-access-token": userId
 				}})
@@ -93,11 +102,28 @@ export default function Coupons({ userId, companyInfo }) {
 					setToastShow(true);
 				});
 
+			await api.get("userAll", {
+				headers: {
+					"x-access-token": userId
+				}})
+				.then((response) => {
+					setUsers(response.data);
+				})
+				.catch((error) => {
+					setTitle("Erro!");
+					if(error.response && typeof(error.response.data) !== "object") {
+						setMessage(error.response.data);
+					} else {
+						setMessage(error.message);
+					}
+					setToastShow(true);
+				});
+
 			setLoading(false);
 		}
 
 		fetchData();
-	});
+	}, []);
 
 	//	Return a list of coupons given type
 	async function handleCouponsList(event, type) {
@@ -111,16 +137,16 @@ export default function Coupons({ userId, companyInfo }) {
 		event.preventDefault();
 
 		const data = {
-			userId: couponUserId,
 			name: couponName,
 			type: couponType,
 			qty: couponQty,
 			method: couponMethod,
 			discount: couponDiscount,
+			minValue: couponMinValue,
 			available: couponAvailable,
+			private: couponPrivate,
+			userId: couponPrivate ? users.find(u => u.email === couponUserId)._id : null
 		};
-
-		console.log(data);
 
 		await api.post("coupon", data, {
 			headers : {
@@ -148,13 +174,15 @@ export default function Coupons({ userId, companyInfo }) {
 		event.preventDefault();
 
 		const data = {
-			userId: couponUserId,
 			name: couponName,
 			type: couponType,
 			qty: couponQty,
 			method: couponMethod,
 			discount: couponDiscount,
+			minValue: couponMinValue,
 			available: couponAvailable,
+			private: couponPrivate,
+			userId: couponPrivate ? users.find(u => u.email === userId)._id : null
 		};
 
 		await api.put("coupon/" + coupon._id, data, {
@@ -255,6 +283,7 @@ export default function Coupons({ userId, companyInfo }) {
 							setCouponType(e.target.value);
 							setCouponMethod(e.target.value === "frete" ? "dinheiro" : couponMethod);
 							setCouponDiscount(companyInfo.freight);
+							setCouponPrivate(e.target.value === "quantidade" ? false : true);
 						}}
 						as="select"
 						placeholder="Tipo do cupom"
@@ -268,7 +297,7 @@ export default function Coupons({ userId, companyInfo }) {
 				</Form.Group>
 			</Row>
 			<Row>
-				<Form.Group as={Col} controlId="couponType" sm>
+				<Form.Group as={Col} controlId="couponMethod" sm>
 					<Form.Label>Método</Form.Label>
 					<Form.Control
 						value={couponType === "frete" ? "dinheiro" : couponMethod}
@@ -279,12 +308,12 @@ export default function Coupons({ userId, companyInfo }) {
 						disabled={couponType === "frete"}
 					>
 						<option>Selecione o método de desconto</option>
-						{couponMethods.map((type, index) => (
-							<option key={index}>{type}</option>
+						{couponMethods.map((method, index) => (
+							<option key={index}>{method}</option>
 						))}
 					</Form.Control>
 				</Form.Group>
-				<Form.Group as={Col} controlId="discount" sm>
+				<Form.Group as={Col} controlId="couponDiscount" sm>
 					<Form.Label>
 						{"Desconto " + (couponMethod === "porcentagem" ? "(em porcentagem %)" : "(em reais R$)")}
 					</Form.Label>
@@ -299,17 +328,43 @@ export default function Coupons({ userId, companyInfo }) {
 				</Form.Group>
 			</Row>
 			<Row>
-				<Form.Group as={Col} controlId="qty" sm>
+				<Form.Group as={Col} controlId="couponQty" sm>
 					<Form.Label>Quantidade</Form.Label>
 					<Form.Control
 						value={couponQty}
 						onChange={e => setCouponQty(e.target.value)}
 						type="number"
 						placeholder="Quantidade"
-						required
+						required={!couponPrivate}
+						disabled={couponPrivate}
 					/>
 				</Form.Group>
-				<Form.Group as={Col} className={modalCouponAdd ? "d-none" : null} controlId="type" sm>
+				<Form.Group as={Col} controlId="couponMinValue" sm>
+					<Form.Label>Valor mínimo (em reais)</Form.Label>
+					<Form.Control
+						value={couponMinValue}
+						onChange={e => setCouponMinValue(e.target.value)}
+						type="number"
+						placeholder="Valor mínimo"
+						required={couponType === "valor"}
+						disabled={couponType !== "valor"}
+					/>
+				</Form.Group>
+			</Row>
+			<Row>
+				<Form.Group as={Col} controlId="type" sm>
+					<Form.Label>Cupom privado?</Form.Label>
+					<Form.Check
+						type={"checkbox"}
+						checked={couponPrivate ? couponPrivate : false}
+						className="my-2"
+						onChange={e => setCouponPrivate(e.target.checked)}
+						label={couponPrivate ? "Privado" : "Público"}
+						required={couponType !== "quantidade"}
+						disabled={couponType === "quantidade"}
+					/>
+				</Form.Group>
+				<Form.Group as={Col} className={modalCouponAdd ? "d-none" : null} controlId="couponAvailable" sm>
 					<Form.Label>Disponibilizar cupom?</Form.Label>
 					<Form.Check
 						type={"checkbox"}
@@ -318,6 +373,24 @@ export default function Coupons({ userId, companyInfo }) {
 						onChange={e => setCouponAvailable(e.target.checked)}
 						label={couponAvailable ? "Disponibilizar" : "Não disponibilizar"}
 					/>
+				</Form.Group>
+			</Row>
+			<Row>
+				<Form.Group as={Col} controlId="couponUserId" sm>
+					<Form.Label>Usuário que receberá o cupom</Form.Label>
+					<Form.Control
+						value={couponUserId}
+						onChange={e => setCouponUserId(e.target.value)}
+						as="select"
+						placeholder="Usuário que receberá o cupom"
+						required={couponPrivate}
+						disabled={!couponPrivate}
+					>
+						<option>Selecione o email do usuário</option>
+						{users.map((user, index) => (
+							<option key={index}>{user.email}</option>
+						))}
+					</Form.Control>
 				</Form.Group>
 			</Row>
 		</>
