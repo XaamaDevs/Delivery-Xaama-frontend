ff6f72fddc96af475aaa2e6e7984d89ddc0b11ef
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-01-20 16:13:11 -0300

    page of rating

---
+++ src/pages/Order/Rating/index.js
@@ -0,0 +1,13 @@
+//	Importing React main module and its features
+import React from "react";
+// import PropTypes from "prop-types";
+
+// // Importing backend api
+// import api from "../../services/api";
+
+//	Exporting resource to routes.js
+export default function Rating() {
+	return (
+		<></>
+	);
+}
