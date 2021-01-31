98d6186ad59c049244c26a57edccd1e2085c6c0c
DiegoTeixeira7 <dieg07teixeira@outlook.com>
2021-01-31 15:16:15 -0300

    Product deck reformulated

--- src/pages/Order/Finish/index.js
+++ src/pages/Order/Finish/index.js
@@ -15,6 +15,7 @@ import {
 	Form,
 	Row,
 	Col,
+	Image,
 	FormGroup,
 	FormLabel,
 	OverlayTrigger,
@@ -26,7 +27,6 @@ import {
 //	Importing website utils
 import Alert from "../../../components/Alert";
 import Push from "../../../components/Push";
-import ProductDeck from "../../../components/ProductDeck";
 import Loading from "../../../components/Loading";
 
 //	Importing React icons features
@@ -36,6 +36,13 @@ import {
 	RiMotorbikeFill,
 	RiCheckLine
 } from "react-icons/ri";
+
+import {
+	BsFillTrashFill
+} from "react-icons/bs";
+
+// Importing image from camera
+import camera from "../../../assets/camera.svg";
 
 // Importing backend api and cep api
 import api from "../../../services/api";
@@ -494,6 +501,111 @@ export default function FinishOrder({
 		);
 	}
 
+	const productDeck = (products, text, bg) => {
+		return (
+			<Tabs fill defaultActiveKey={0} id="productDeck">
+				{products.map((product, index) => (
+					<Tab key={index} eventKey={index} title={product.product.name}>
+						<Card className="h-100 p-1" text={text ? text : "dark"} bg={bg ? bg : "light"} key={product._id}>
+							<Row>
+								<Col className="d-flex text-center" sm="6">
+									<Image
+										className={product.product.thumbnail ? "w-100 m-auto" : "w-75 m-auto"}
+										src={
+											product.product.thumbnail ?
+												process.env.REACT_APP_API_URL + "files/" + product.product.thumbnail
+												:
+												camera
+										}
+										alt="thumbnail"
+										fluid
+										rounded
+									/>
+								</Col>
+								<Col sm>
+									<Row>
+										<Card.Body>
+											<Row>
+												<Col>
+													<Card.Title>{product.product ? product.product.name : null }</Card.Title>
+												</Col>
+												<Col align="right">
+													<BsFillTrashFill 
+														className="my-0 mx-2 text-danger" size="20"
+														style={{cursor: "pointer"}}
+														onClick={null} />
+												</Col>
+											</Row>
+											
+											<Card.Text>
+												{product.product && product.product.ingredients ?
+													((product.product.ingredients.length === 1) ?
+														"Ingrediente: "
+														:
+														"Ingredientes: "
+													)
+													:
+													null
+												}
+												{product.product && product.product.ingredients ?
+													product.product.ingredients.map((ingredient, index) => (
+														index === product.product.ingredients.length-1 ?
+															ingredient
+															:
+															ingredient + ", "
+													))
+													:
+													null
+												}
+											</Card.Text>
+										</Card.Body>
+									</Row>
+									<Row>
+										<Card.Body>
+											<Card.Title>Observações:</Card.Title>
+											<Card.Text>
+												{product.note ? product.note : "Sem Observações"}
+											</Card.Text>
+										</Card.Body>
+									</Row>
+								</Col>
+							</Row>
+							<Row>
+								<Col>
+									<Card.Body>
+										<Card.Title>{product.additions.length ? "Adições:" : "Sem Adições"}</Card.Title>
+										{product.additions.length ?
+											<Card.Text>
+												<Row>
+													{(product.additions).map(addition => (
+														<Col key={(addition) ? addition._id : null } sm>
+															{addition.name + "\nPreço: R$" + addition.price}
+														</Col>
+													))}
+												</Row>
+											</Card.Text>
+											:
+											null
+										}
+									</Card.Body>
+								</Col>
+							</Row>
+							<Card.Footer>
+								<small>
+									{product.product ?
+										"Preço: R$" + product.product.prices[product.size]
+										:
+										null
+									}
+								</small>
+							</Card.Footer>
+						</Card>
+					</Tab>
+				))}
+			</Tabs>
+		);
+	};
+
 	return (
 		<>
 			<Container className="bg-light text-dark rounded my-auto">
@@ -549,7 +661,7 @@ export default function FinishOrder({
 												{isLoading ?
 													<Loading animation="grow" />
 													:
-													<ProductDeck products={orderDeliverProducts} />
+													productDeck(orderDeliverProducts)
 												}
 											</Col>
 										</Row>
